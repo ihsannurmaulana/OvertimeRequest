71276cb62a0f953e35f3bcdb780b1fb68123ff7c
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-07-22 23:08:49 +0700

    Add Client

---
+++ ClientOvertime/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClientOvertime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
