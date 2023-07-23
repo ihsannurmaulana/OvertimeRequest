140a17f26f39a9c46846af1e35ce9e3ba7390147
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-07-23 09:07:37 +0700

    feat: create payslip repository and interface

---
+++ API/Repositories/PayslipRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class PayslipRepository : GeneralRepository<Payslip>, IPayslipRepository
+{
+    public PayslipRepository(OvertimeDbContext context) : base(context) { }
+}
