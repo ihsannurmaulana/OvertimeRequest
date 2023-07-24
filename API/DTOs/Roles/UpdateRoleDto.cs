ce640f7516ccb3aa9b6b1e752c8c9170a4c9461a
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-07-24 16:15:10 +0700

    feat: create role dto

---
+++ API/DTOs/Roles/UpdateRoleDto.cs
@@ -0,0 +1,27 @@
+﻿using API.Models;
+
+namespace API.DTOs.Roles;
+
+public class UpdateRoleDto
+{
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+
+    public static implicit operator Role(UpdateRoleDto updateRoleDto)
+    {
+        return new()
+        {
+            Guid = updateRoleDto.Guid,
+            Name = updateRoleDto.Name
+        };
+    }
+
+    public static explicit operator UpdateRoleDto(Role role)
+    {
+        return new()
+        {
+            Guid = role.Guid,
+            Name = role.Name
+        };
+    }
+}
