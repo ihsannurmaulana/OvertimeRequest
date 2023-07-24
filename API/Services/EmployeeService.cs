93278c1cd2479da626398a200544a300792bdc78
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-07-24 16:47:27 +0700

    feat: create service employee

---
+++ API/Services/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using API.Contracts;
+using API.DTOs.Employees;
+
+namespace API.Services;
+
+public class EmployeeService
+{
+    private readonly IEmployeeRepository employeeRepository;
+
+    public EmployeeService(IEmployeeRepository employeeRepository)
+    {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public IEnumerable<GetEmployeeDto> GetEmployee()
+    {
+        var employees = employeeRepository.GetAll();
+
+    }
+}
