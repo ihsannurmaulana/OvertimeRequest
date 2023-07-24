f827910e5150d0279d880296b43aab45eb3e94f0
dikaadaud <andika.despandi1@gmail.com>
2023-07-24 11:45:58 +0700

    feat : adding property overtime dto

--- API/DTOs/Overtimes/GetOvertimeDTO.cs
+++ API/DTOs/Overtimes/GetOvertimeDTO.cs
@@ -1,5 +1,17 @@
-﻿namespace API.DTOs.Overtimes;
+﻿using API.Utilities.Enums;
+
+namespace API.DTOs.Overtimes;
 
 public class GetOvertimeDTO
 {
+    public Guid Guid { get; set; }
+    public int OvertimeNumber { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Remarks { get; set; }
+    public StatusLevel Status { get; set; }
+    public Guid EmployeeGuid { get; set; }
+    public Guid PayslipGuid { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
 }
