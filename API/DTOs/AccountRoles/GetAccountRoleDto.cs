d4784006d17781f93beeee01994246df2f139cfc
Saryuns <sarahayunilams@gmail.com>
2023-07-24 16:36:12 +0700

    feat: create account role dto

---
+++ API/DTOs/AccountRoles/GetAccountRoleDto.cs
@@ -0,0 +1,28 @@
+﻿using API.Models;
+
+namespace API.DTOs.AccountRoles;
+
+public class GetAccountRoleDto
+{
+    public Guid? AccountGuid { get; set; }
+
+    public Guid? RoleGuid { get; set; }
+
+    public static implicit operator AccountRole(GetAccountRoleDto getAccountRoleDto)
+    {
+        return new()
+        {
+            AccountGuid = getAccountRoleDto.AccountGuid,
+            RoleGuid = getAccountRoleDto.RoleGuid
+        };
+    }
+
+    public static explicit operator GetAccountRoleDto(AccountRole accountRole)
+    {
+        return new()
+        {
+            AccountGuid = accountRole.AccountGuid,
+            RoleGuid = accountRole.RoleGuid
+        };
+    }
+}
---
+++ API/DTOs/AccountRoles/UpdateAccountRoleDto.cs
@@ -0,0 +1,28 @@
+﻿using API.Models;
+
+namespace API.DTOs.AccountRoles;
+
+public class UpdateAccountRoleDto
+{
+    public Guid? AccountGuid { get; set; }
+
+    public Guid? RoleGuid { get; set; }
+
+    public static implicit operator AccountRole(UpdateAccountRoleDto updateAccountRoleDto)
+    {
+        return new()
+        {
+            AccountGuid = updateAccountRoleDto.AccountGuid,
+            RoleGuid = updateAccountRoleDto.RoleGuid
+        };
+    }
+
+    public static explicit operator UpdateAccountRoleDto(AccountRole accountRole)
+    {
+        return new()
+        {
+            AccountGuid = accountRole.AccountGuid,
+            RoleGuid = accountRole.RoleGuid
+        };
+    }
+}
