74286bd4c9a0de67d60291202a99506633dcfc50
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-07-24 16:06:37 +0700

    feat: create hostory dto

---
+++ API/DTOs/Histories/UpdateHistoryDto.cs
@@ -0,0 +1,28 @@
+﻿using API.Models;
+
+namespace API.DTOs.Histories;
+
+public class UpdateHistoryDto
+{
+    public Guid Guid { get; set; }
+
+    public Guid OvertimeGuid { get; set; }
+
+    public static implicit operator History(UpdateHistoryDto updateHistoryDto)
+    {
+        return new()
+        {
+            Guid = updateHistoryDto.Guid,
+            OvertimeGuid = updateHistoryDto.OvertimeGuid
+        };
+    }
+
+    public static explicit operator UpdateHistoryDto(History history)
+    {
+        return new()
+        {
+            Guid = history.Guid,
+            OvertimeGuid = history.OvertimeGuid
+        };
+    }
+}
