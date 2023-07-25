bdc3079421e708bc24e48cb226855935f64f56f6
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-07-25 12:41:37 +0700

    feat : adding login validator

---
+++ API/Utilities/Validations/LoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using API.DTOs.Accounts;
+using FluentValidation;
+
+namespace API.Utilities.Validations;
+
+public class LoginValidator : AbstractValidator<LoginDto>
+{
+    public LoginValidator()
+    {
+        RuleFor(p => p.Email)
+           .NotEmpty()
+           .EmailAddress();
+
+        RuleFor(p => p.Password)
+           .NotEmpty();
+    }
+}
