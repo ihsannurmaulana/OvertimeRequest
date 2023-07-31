36e15a879453f3ef32f594b7f084e9c1cab1d3c5
ihsannurmaulana <81961193+ihsannurmaulana@users.noreply.github.com>
2023-07-31 10:43:15 +0700

    Revert "feat: add login dashboard"

--- ClientOvertime/Contracts/IAccountRepository.cs
+++ ClientOvertime/Contracts/IAccountRepository.cs
@@ -7,6 +7,4 @@ namespace ClientOvertime.Contracts;
 public interface IAccountRepository : IGeneralRepository<AccountVMGet, string>
 {
     Task<ResponseHandler<EmployeeVM>> CreateAccount(EmployeeVM entity);
-
-    Task<ResponseHandler<string>> Login(AccountVMLogin accountVMLogin);
 }
--- ClientOvertime/Program.cs
+++ ClientOvertime/Program.cs
@@ -1,37 +1,14 @@
-using API.Services;
 using ClientOvertime.Contracts;
 using ClientOvertime.Repositories;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddSession();
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 builder.Services.AddScoped<IAccountRoleRepository, AccountRoleRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-
-// Jwt Configuration
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-       .AddJwtBearer(options =>
-       {
-           options.RequireHttpsMetadata = false;
-           options.SaveToken = true;
-           options.TokenValidationParameters = new TokenValidationParameters()
-           {
-               ValidateIssuer = true,
-               ValidIssuer = builder.Configuration["JWTService:Issuer"],
-               ValidateAudience = true,
-               ValidAudience = builder.Configuration["JWTService:Audience"],
-               IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTService:Key"])),
-               ValidateLifetime = true,
-               ClockSkew = TimeSpan.Zero
-           };
-       });
 
 var app = builder.Build();
 
@@ -42,21 +19,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseSession();
-
-//Add JWToken to all incoming HTTP Request Header
-app.Use(async (context, next) =>
-{
-    var JWToken = context.Session.GetString("JWToken");
-
-    if (!string.IsNullOrEmpty(JWToken))
-    {
-        context.Request.Headers.Add("Authorization", "Bearer " + JWToken);
-    }
-
-    await next();
-});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
